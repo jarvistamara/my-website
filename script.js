6708051486c51558df777a61ada20b253b58b02c
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-07 23:43:51 -0500

    added background image

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+/* CLASS ASSIGNMENT
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
-document.querySelector('body').appendChild(h2);
+document.querySelector('body').appendChild(h2); */
+
+
